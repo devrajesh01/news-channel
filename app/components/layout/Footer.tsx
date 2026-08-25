b17acbca18ff25eae91caa6f9680503c59aa5bfa
rajesh <rajesh.kumar.dev23@gmail.com>
2026-08-25 17:51:05 +0530

    phase 1 configartion: next js , tailwind css, theme colors , theme mode ,

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,23 @@
+export default function Footer() {
+  return (
+    <footer className="mt-20 border-t border-[var(--border)]">
+      <div className="mx-auto max-w-7xl px-5 py-10">
+        <div className="flex flex-col justify-between gap-5 md:flex-row">
+          <div>
+            <h2 className="font-[var(--font-newsreader)] text-2xl font-bold">
+              NEWS<span className="text-[var(--accent)]">.</span>
+            </h2>
+
+            <p className="mt-2 max-w-md text-sm text-[var(--muted)]">
+              Independent journalism, breaking news and stories that matter.
+            </p>
+          </div>
+
+          <p className="text-sm text-[var(--muted)]">
+            © 2026 News Channel. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
---
+++ app/components/theme/ThemeToggle.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const [dark, setDark] = useState(false);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+      setDark(true);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const html = document.documentElement;
+
+    if (html.classList.contains("dark")) {
+      html.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+      setDark(false);
+    } else {
+      html.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+      setDark(true);
+    }
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      aria-label="Toggle theme"
+      className="flex h-9 w-9 items-center justify-center cursor-pointer text-white dark:text-red-500  rounded-full border border-[var(--border)] transition hover:bg-[var(--surface)]"
+    >
+      {dark ? "☀" : "☾"}
+    </button>
+  );
+}
